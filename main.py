a31cd7409b0d9cd2826d2154ea66515ddc0b3496
Anson <10340400+anson_z@user.noreply.gitee.com>
2025-05-26 15:27:09 +0800

    swagger 依赖的前端资源直接加载本地的静态文文件

--- main.py
+++ main.py
@@ -17,6 +17,34 @@ from routers import (
     allure_api
 )
 
+# 定义常量用于静态文件路径
+STATIC_DIR = 'static'
+
+
+def use_local_static():
+    from fastapi.openapi.docs import get_swagger_ui_html, get_redoc_html
+
+    # 覆盖原有方法，使得swagger从本地加载静态资源
+    def custom_get_swagger_ui_html(*args, **kwargs):
+        kwargs["swagger_js_url"] = f"/{STATIC_DIR}/swagger-ui/swagger-ui-bundle.js"
+        kwargs["swagger_css_url"] = f"/{STATIC_DIR}/swagger-ui/swagger-ui.css"
+        kwargs["swagger_favicon_url"] = f"/{STATIC_DIR}/swagger-ui/favicon.jpg"
+        return get_swagger_ui_html(*args, **kwargs)
+
+    def custom_get_redoc_html(*args, **kwargs):
+        kwargs["redoc_js_url"] = f"/{STATIC_DIR}/redoc/bundles/redoc.standalone.js"
+        kwargs["redoc_favicon_url"] = f"/{STATIC_DIR}/redoc/favicon.png"
+        return get_redoc_html(*args, **kwargs)
+
+    # 替换原有方法
+    sys.modules["fastapi.openapi.docs"].get_swagger_ui_html = custom_get_swagger_ui_html
+    sys.modules["fastapi.openapi.docs"].get_redoc_html = custom_get_redoc_html
+
+
+# 使用本地静态文件配置 Swagger UI 和 ReDoc
+use_local_static()
+
+
 # 初始化FastAPI应用程序
 app = FastAPI(
     swagger_ui_parameters=None,
