6f977ee4f51f8cc58f353b5118029470d6ed2bd8
郑雪峰 <zhengxuefeng@zhengxuefengdeMacBook-Pro.local>
2025-05-26 17:25:17 +0800

    feat:新增结算业务用例

--- testcases/fin/test_sett_busi_check.py
+++ testcases/fin/test_sett_busi_check.py
@@ -28,37 +28,87 @@ class TestSettBusiCheck(BaseTest):
         cls.yaml_util = YamlUtil()  
         cls.fin_path = cls.yaml_util.read_yaml(cls.base_api_path).get("apis", {})
         cls.fin_params = cls.yaml_util.read_yaml(cls.base_api_params).get("api_params", {})  
-        
-    def test_sett_item_record(self):
-        """测试结算项对账确认"""
-        # 调用 test_add_sett_item 方法 新建一条结算项使用新建结算项进行对账确认
+    
+    
+    def get_sett_item_id(self):
+        """获取不同状态的结算项ID 已创建，已对账，已汇单 """
+         # 调用 test_add_sett_item 方法 新建一条结算项使用新建结算项进行对账确认
         sett_item = TestSettlementItem()
         sett_item.setup_class()
-        sett_item_id = sett_item.test_add_sett_item()
+        created_sett_item_id = sett_item.test_add_sett_item()
         
-        url = self.fin_path["SETT-ITEM-结算项确认及汇单-关联操作-异步服务"]["path"]
-        data = self.fin_params.get(url, {})
-        data["params"]["request"][0]["id"] = sett_item_id
-        result=self.http.post(url, json=data, description="结算项对账确认")
-        
-        #等待异步任务执行
-        time.sleep(3)  # 等待3秒
-        
-        sql=f"""
-            select id, sett_item_status, async_execution_status, sett_doc_id
+        sett_doc_created_sql="""
+            select id
             from sett_item_tr where deleted=0
-            and id={sett_item_id};
+            and sett_item_status='SETT_DOC_CREATED'
+            order by created_at desc;
         """
-        sql_result=self.db.query(sql)
-        self.assert_util.assert_response_success(result)
-        self.assert_util.assert_eq(sql_result[0]["sett_item_status"], "SETT_DOC_CREATED")
-        self.assert_util.assert_eq(sql_result[0]["async_execution_status"], "SUCCEEDED")
-        self.assert_util.assert_not_empty(sql_result[0]["sett_doc_id"], "结算单号为空")
         
+        doc_created_sett_item_id=self.db.query(sett_doc_created_sql)[0]["id"]
+        
+        return [created_sett_item_id, doc_created_sett_item_id]
+    
+    @allure.title("结算项对账确认")
+    @allure.description("1、新建结算项\n2、对账确认\n3、检查结算单是否生成")
+    def test_sett_item_record(self):
+        """测试结算项对账确认"""
+        # 获取结算项ID列表
+        sett_item_ids = self.get_sett_item_id()
+        
+        # 遍历每个ID进行测试
+        for index, sett_item_id in enumerate(sett_item_ids):
+            url = self.fin_path["SETT-ITEM-结算项确认及汇单-关联操作-异步服务"]["path"]
+            data = self.fin_params.get(url, {})
+            data["params"]["request"][0]["id"] = sett_item_id
+            result = self.http.post(url, json=data, description=f"结算项对账确认 - ID: {sett_item_id}")
+            
+            #等待异步任务执行
+            time.sleep(3)  # 等待3秒
+            
+            sql = f"""
+                select id, sett_item_status, async_execution_status, sett_doc_id
+                from sett_item_tr where deleted=0
+                and id={sett_item_id};
+            """
+            sql_result = self.db.query(sql)
+            
+            if index == 0:  # 第一个ID的断言
+                self.assert_util.assert_response_success(result)
+                self.assert_util.assert_eq(sql_result[0]["sett_item_status"], "SETT_DOC_CREATED")
+                self.assert_util.assert_eq(sql_result[0]["async_execution_status"], "SUCCEEDED")
+                self.assert_util.assert_not_empty(sql_result[0]["sett_doc_id"], "结算单号为空")
+            elif index == 1:  # 第二个ID的断言
+                assert result.get("success",{}) == False
+                assert result.get("err",{}).get("msg",{}) == "结算单异步任务提交失败，请确认结算单异步执行状态！"
+                self.assert_util.assert_eq(sql_result[0]["sett_item_status"], "SETT_DOC_CREATED")
+                self.assert_util.assert_eq(sql_result[0]["async_execution_status"], "SUCCEEDED")
+                self.assert_util.assert_not_empty(sql_result[0]["sett_doc_id"], "结算单号为空")
+    
+    #缺少已对账情况，待补充            
+    def test_sett_item_manual_remittance(self):
+        """测试结算项手工汇单"""
+        url = self.fin_path["SETT-ITEM-结算项手工汇单-关联操作-异步服务"]["path"]
+        sett_item_ids = self.get_sett_item_id()
+        for index, sett_item_id in enumerate(sett_item_ids):
+            data = self.fin_params.get(url, {})
+            data["params"]["request"][0]["id"] = sett_item_id
+            result = self.http.post(url, json=data, description=f"结算项手工汇单 - ID: {sett_item_id}")
+            time.sleep(3)  # 等待3秒
+            
+            sql = f"""
+                select id, sett_item_status, async_execution_status, sett_doc_id
+                from sett_item_tr where deleted=0
+                and id={sett_item_id};
+            """
+            sql_result = self.db.query(sql)
+            if index == 0 or index == 1:  # 第1.2个ID的断言
+                assert result.get("success",{}) == False
+                assert result.get("err",{}).get("msg",{}) == "结算单异步任务提交失败，请确认结算单异步执行状态！"
 if __name__ == "__main__":
     test = TestSettBusiCheck()
     test.setup_class()
     test.test_sett_item_record()
+    test.test_sett_item_manual_remittance()
 
         
         
