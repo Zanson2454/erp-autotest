1f4142ad95595e2a754a76c77d021453661ae5a1
mckay <wangshengjun@terminus.io>
2025-06-19 16:42:14 +0800

    fix: 修复生产基础测试类中的导入语句，使用report_util替代allure_simple

--- testcases/prd/basic/__init__.py
+++ testcases/prd/basic/__init__.py
@@ -12,7 +12,7 @@
 import allure
 from testcases.base_test import BaseTest
 from utils.param_util import ParamUtil
-from utils.allure_simple import a
+from utils.report_util import a
 from .init_config import PrdConfigInitializer
 
 @allure.epic("生产管理")
