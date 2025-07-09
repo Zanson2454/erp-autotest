b3c7b2425b41d72462d4cedab4450ed99eef63c3
Anson <10340400+anson_z@user.noreply.gitee.com>
2025-07-09 14:26:34 +0800

    模型系统服务管理 调试通过

--- testcases/gen_md/gen_base/test_model_system_management.py
+++ testcases/gen_md/gen_base/test_model_system_management.py
@@ -47,38 +47,27 @@ class TestModelSystemManagement(GenMdBaseTest):
             api_path = self.get_api_path("模型系统分页查询服务")
             params, url = self.get_api_params(api_path)
 
-            filtered_params = ParamUtil.filter_post_body_fields(
-                params, ["pageable", "fields", "systemParams"], ["params", "request"]
-            )
-            set_dict = {
-                "pageable": {
-                    "pageNo": 1,
-                    "pageSize": 20,
-                    "needTotal": True,
-                    "sortOrders": None,
-                    "conditionItems": None
-                },
-                "fields": [
-                    {"name": "id", "type": "TEXT"},
-                    {"name": "modelKey", "type": "TEXT"},
-                    {"name": "modelName", "type": "TEXT"},
-                    {"name": "modelType", "type": "TEXT"}
-                ],
-                "systemParams": None
+            params['params']={
+                "request": {
+                    "modelKey": "GEN_MD$gen_coun_type_cf",
+                    "pageable": {
+                        "pageNo": "1",
+                        "pageSize": "10"
+                    }
+                }
             }
-            ParamUtil.set_request_params(filtered_params, set_dict)
 
-            response = self.http.post(url, json=filtered_params)
+            response = self.http.post(url, json=params)
             self.assert_util.assert_response_data(response)
 
             # 保存模型系统ID用于后续测试
-            data_list = response.get("data", {}).get("data", {}).get("records", [])
+            data_list = response.get("data", {}).get("data", {}).get("data", []).get("data", [])
             if data_list:
                 self.model_system_id = data_list[0].get("id")
                 # 收集多个ID用于根据ID集合查询测试
                 self.model_system_ids = [item.get("id") for item in data_list[:3] if item.get("id")]
 
-            a.json(filtered_params, "请求数据")
+            a.json(params, "请求数据")
             a.json(response, "响应数据")
 
         except Exception as e:
@@ -96,30 +85,24 @@ class TestModelSystemManagement(GenMdBaseTest):
     def test_query_model_system_by_ids(self):
         """模型根据ID集合查询详情用例 - GEN_MODEL_SYSTEM_QUERY_BY_IDS_ACTION_SERVICE"""
         try:
-            if not self.model_system_ids:
+            if not self.model_system_id:
                 self.test_query_model_system_page()
 
-            # 如果分页查询没有返回数据，使用模拟ID
-            if not self.model_system_ids:
-                self.logger.warning("未获取到模型系统ID，使用模拟ID进行测试")
-                self.model_system_ids = [1, 2, 3]
-
+    
             api_path = self.get_api_path("模型根据ID集合查询详情服务")
             params, url = self.get_api_params(api_path)
 
             filtered_params = ParamUtil.filter_post_body_fields(
-                params, ["ids"], ["params", "request"]
+                params, ["ids","modelKey"], ["params", "request"]
             )
-            set_dict = {"ids": self.model_system_ids}
+            set_dict = {
+                "ids": [self.model_system_id],
+                "modelKey":"GEN_MD$gen_coun_type_cf"
+                }
             ParamUtil.set_request_params(filtered_params, set_dict)
 
             response = self.http.post(url, json=filtered_params)
             self.assert_util.assert_response_data(response)
-
-            # 验证返回的数据结构
-            data_list = response.get("data", {}).get("data", [])
-            if data_list:
-                self.assert_util.assert_by_operator(len(data_list), ">=", 1, "应该返回至少一条模型系统数据")
 
             a.json(filtered_params, "请求数据")
             a.json(response, "响应数据")
