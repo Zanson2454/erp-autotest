712cebabbc3349246e61a50ab4f87bcbaa23f203
Anson <10340400+anson_z@user.noreply.gitee.com>
2025-12-04 19:25:05 +0800

    fix: 路径引用错误

--- testcases/scm_sls/so_01_crud/test_so_delete.py
+++ testcases/scm_sls/so_01_crud/test_so_delete.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import sys
 
 # 添加项目根目录到 Python 路径
-project_root = Path(__file__).resolve().parent.parent.parent.parent
+project_root = Path(__file__).resolve().parent.parent.parent
 sys.path.insert(0, str(project_root))
 
 from utils.report_util import a, case_decorator
